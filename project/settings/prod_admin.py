0d97ba9864ec5bb8e5eebcaa144d4616ae4fedb7
Hamdy <hamdyadam97@gmail.com>
2025-07-05 20:05:28 +0300

    alllwed ip

--- project/settings/prod_admin.py
+++ project/settings/prod_admin.py
@@ -1,7 +1,7 @@
 from .dev import *
 ROOT_URLCONF = 'project.urls_admin'
-ALLOWED_HOSTS = ['admin.awael-sa.com', 'www.admin.awael-sa.com',  'https://admin.awael-sa.com',
-    'https://www.admin.awael-sa.com',]
+ALLOWED_HOSTS = ['admin.awael-sa.com', 'www.admin.awael-sa.com',  'https://admin.awael-sa.com','168.231.101.215',
+                 'https://www.admin.awael-sa.com',]
 SESSION_COOKIE_DOMAIN = '.awael-sa.com'
 # CSRF_TRUSTED_ORIGINS = ['https://admin.awael-sa.com']
 
--- project/settings/prod_frontend.py
+++ project/settings/prod_frontend.py
@@ -1,8 +1,8 @@
 from .prod import *
 
 ROOT_URLCONF = 'project.urls_frontend'
-ALLOWED_HOSTS = ['awael-sa.com', 'www.awael-sa.com',  'https://awael-sa.com',
-    'https://www.awael-sa.com',]
+ALLOWED_HOSTS = ['awael-sa.com', 'www.awael-sa.com',  'https://awael-sa.com','168.231.101.215',
+                 'https://www.awael-sa.com',]
 SESSION_COOKIE_DOMAIN = '.awael-sa.com'
 # CSRF_TRUSTED_ORIGINS = ['https://www.awael-sa.com']
 

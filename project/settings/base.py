95f3b9664beb5e9bcb350d8c729c5a13efcbcfe8
Hamdy <hamdyadam97@gmail.com>
2025-09-06 14:52:32 +0300

    change number

--- project/settings/base.py
+++ project/settings/base.py
@@ -15,8 +15,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
-#
-# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- project/settings/dev_admin.py
+++ project/settings/dev_admin.py
@@ -1,6 +1,6 @@
 from .dev import *
 ROOT_URLCONF = 'project.urls_admin'
-ALLOWED_HOSTS = ['admin.localhost', 'localhost']
+ALLOWED_HOSTS = []
 SESSION_COOKIE_DOMAIN = 'admin.localhost'
 CSRF_TRUSTED_ORIGINS = ['http://admin.localhost:8001']
 
